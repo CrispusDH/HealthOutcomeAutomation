186761ef46f10d3bb41cd40456dbaefda3f2943c
CrispusDH <qacrispus@gmail.com>
2017-05-08 18:14:46 +0300

    Add @Step annotation (v.1)

--- src/main/java/wrappers/BasePage.java
+++ src/main/java/wrappers/BasePage.java
@@ -3,7 +3,11 @@ package wrappers;
 import enums.ConditionTabsEnum;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import pages.*;
+import pages.SpecificTreatmentVideosPage;
+import pages.TreatmentRatingsConditionPage;
+import pages.TreatmentReviewsPage;
+import pages.TreatmentVideosPage;
+import ru.yandex.qatools.allure.annotations.Step;
 import utility.ReadXMLFile;
 import waiters.WaitConditionForUrl;
 import waiters.WaitConditions;
@@ -13,13 +17,11 @@ import java.util.List;
 import java.util.Objects;
 
 import static enums.ConditionTabsEnum.*;
-import static javaslang.API.$;
-import static javaslang.API.Case;
-import static javaslang.API.Match;
+import static javaslang.API.*;
+import static org.joor.Reflect.on;
 import static utility.Configuration.CoreConstants.WAIT_TIMEOUT;
 import static waiters.WaitConditionForUrl.urlToBe;
 import static waiters.WaitConditions.*;
-import static org.joor.Reflect.*;
 
 public abstract class BasePage {
 
@@ -31,32 +33,39 @@ public abstract class BasePage {
         this.wait = new WebDriverWait(driver, WAIT_TIMEOUT);
     }
 
+    @Step("Wait for {0} locator using {1} condition")
     @SuppressWarnings("unchecked")
     protected <T> T waitFor(By locator, WaitConditions condition) {
         return (T) wait.until(condition.getType().apply(locator));
     }
 
+    @Step("Wait for {0} locator using {1} condition")
     @SuppressWarnings("unchecked")
     protected <T> T waitForElements(By locator, WaitConditions condition){
         return (T) wait.until(condition.getType().apply(locator));
     }
 
+    @Step("Click on {0} locator using enabled condition")
     protected void click(By locator) {
         click(locator, enabled);
     }
 
+    @Step("Click on {0} locator using {1} condition")
     protected void click(By locator, WaitConditions condition) {
         ((WebElement) waitFor(locator, condition)).click();
     }
 
+    @Step("Type in {0} locator the {1} text")
     protected void type(By locator, CharSequence text) {
         ((WebElement) waitFor(locator, visible)).sendKeys(text);
     }
 
+    @Step("Click on {0} webElement")
     protected void click(WebElement webElement){
         webElement.click();
     }
 
+    @Step("Find element by {1} text")
     protected WebElement findElementByText(List<WebElement> webElements, String text) {
         return webElements
                 .stream()
@@ -65,14 +74,17 @@ public abstract class BasePage {
                 .orElseThrow(() -> new NoSuchElementException(webElements.size() + " size, text - " + text));
     }
 
+    @Step("Get URL")
     protected String getURL(){
         return this.driver.getCurrentUrl();
     }
 
+    @Step("Check URL")
     protected boolean checkUrlToBe(String sExpectedUrl, WaitConditionForUrl condition){
         return wait.until(condition.getType().apply(sExpectedUrl));
     }
 
+    @Step("Check URL for {0} page")
     protected boolean checkUrlToBe(String sPageName){
         return checkUrlToBe(ReadXMLFile.takeConstantFromXML("URL", sPageName, "url"), urlToBe);
     }
@@ -104,12 +116,14 @@ public abstract class BasePage {
         return on(pageClass).create().get();
     }
 
+    @Step("Click on {0} locator")
     protected <T extends BasePage> T clickOnOption(By by, Class<T> pageClass){
         click((WebElement) waitFor(by, enabled));
         return on(pageClass).create().get();
     }
 
     //return specific (T) Page class using tabLocator and Matching by condition tabs enum value
+    @Step("Click on {0} tab, by {1} locator")
     @SuppressWarnings("unchecked")
     protected <T extends BasePage> T clickOnSpecificTab(ConditionTabsEnum conditionTabsEnum, By tabLocator){
         return (T) Match(conditionTabsEnum).of(
